30296751fffcea63d38f538aced870c8d7c2299d
Petar Nikolov <penikolov23@gmail.com>
2018-12-26 20:29:08 +0200

    [Player] Limit the playing field to the camera's view
    The player cannot exit the playing field i.e. it cannot exit the main
    camera's view.
    
    The player's movement and collision detection with the playing field
    borders has been achieved via Unity's built-in physics engine. The player
    movement script has been altered to use said physics system.
    
    A singleton game manager class was to be used to implement the changes.
    However, it is not needed as of this time. It has been left for future
    development.

---
+++ Assets/Scripts/GameManager/PersistantGameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PersistantGameManager : MonoBehaviour 
+{
+    public static PersistantGameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,31 +1,39 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour 
 {
-    public int horizontalMovementSpeed = 16;
-    public int verticalMovementSpeed = 10;
+    public float horizontalMovementSpeed = 16f;
+    public float verticalMovementSpeed = 10f;
+    private Rigidbody2D rb2D;
 
-	private void Update() 
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
     {
         float horizontalAxisInput = Input.GetAxisRaw("Horizontal");
         float verticalAxisInput = Input.GetAxisRaw("Vertical");
+        Vector2 velocity = new Vector2(0, 0);
 
         if (horizontalAxisInput == 1)
         {
-            transform.Translate(Vector2.right * horizontalMovementSpeed * Time.deltaTime);
+            velocity[0] = horizontalMovementSpeed;
         }
-        else if (horizontalAxisInput == -1) {
-            transform.Translate(Vector2.left * horizontalMovementSpeed * Time.deltaTime);
+        else if (horizontalAxisInput == -1) 
+        {
+            velocity[0] = -horizontalMovementSpeed;
         }
-
         if (verticalAxisInput == 1)
         {
-            transform.Translate(Vector2.up * verticalMovementSpeed * Time.deltaTime);
+            velocity[1] = verticalMovementSpeed;
         }
-        else if (verticalAxisInput == -1) {
-            transform.Translate(Vector2.down * verticalMovementSpeed * Time.deltaTime);
+        else if (verticalAxisInput == -1) 
+        {
+            velocity[1] = -verticalMovementSpeed;
         }
-	}
+        rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+    }
 }
 
