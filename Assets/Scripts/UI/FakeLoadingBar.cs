230a902989c091c22b4e5614f510504288b3a868
Petar Nikolov <penikolov23@gmail.com>
2018-12-28 01:41:28 +0200

    [UI] Add intermission scene
    The intermission scene should be used to switch between and introduce levels.
    So far, only the scene's UI elements have been modeled (i.e. it does not
    contain any logic for switching to and from the intermission scene).

---
+++ Assets/Scripts/UI/FakeLoadingBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FakeLoadingBar : MonoBehaviour
+{
+    public float timeToDisplay = 1f;
+    public Text textBox;
+    public char borderCharacter = '|';
+    public char progressCharacter = '#';
+    public int numberOfProgressCharacters = 10;
+
+    void Awake()
+    {
+        StartCoroutine(StartLoading());
+    }
+
+    IEnumerator StartLoading()
+    {
+        int progressBarLength = numberOfProgressCharacters + 2;
+        string progressBar = new string(borderCharacter, 2);
+        float characterTimeFraction = timeToDisplay / (progressBarLength + 1);
+
+        for (int i = 0; i < progressBarLength + 1; i++)
+        {
+            textBox.text = progressBar;
+            progressBar = progressBar.Insert(1, progressCharacter.ToString());
+            yield return new WaitForSeconds(characterTimeFraction);
+        }
+    }
+}
+
