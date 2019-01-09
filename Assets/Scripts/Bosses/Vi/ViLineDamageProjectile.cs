acfac60e205969d1fcd2cd32eeb9265da2a7a2d5
Petar Nikolov <penikolov23@gmail.com>
2019-01-09 13:30:34 +0200

    [Level/Demo] Add pre destruction VFX to line damage projectiles
    Vi's line damage projectiles start an animation 0.5 seconds before they
    explode, indicating their destruction to the player.

--- Assets/Scripts/Bosses/Vi/ViLineDamageProjectile.cs
+++ Assets/Scripts/Bosses/Vi/ViLineDamageProjectile.cs
@@ -6,11 +6,14 @@ public class ViLineDamageProjectile : MonoBehaviour
     public string tagToHit;
     public float minTriggerDelayTime = 0.5f;
     public float maxTriggerDelayTime = 1.5f;
+    public Animator animator;
+    public float preDestructionVFXDuration = 0.5f;
 
     void Start()
     {
         float triggerDelayTime = Random.Range(minTriggerDelayTime, maxTriggerDelayTime);
 
+        Invoke("StartPreDestructionVFX", triggerDelayTime - preDestructionVFXDuration);
         Invoke("SpawnDamageLine", triggerDelayTime);
     }
 
@@ -20,6 +23,11 @@ public class ViLineDamageProjectile : MonoBehaviour
         {
             SpawnDamageLine();
         }
+    }
+
+    private void StartPreDestructionVFX()
+    {
+        animator.SetTrigger("StartExplosion");
     }
 
     private void SpawnDamageLine()
