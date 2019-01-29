7986c711b7cc6b747cb671a086d13cc1e7e84524
Petar Nikolov <penikolov23@gmail.com>
2019-01-29 14:15:30 +0200

    [Refactoring] Document Vi's movement related scripts
    The behaviour of Vi's movement related scripts has not been altered.
    They have only received improvements to code quality and documentation.

--- Assets/Scripts/Bosses/Vi/ViMovement.cs
+++ Assets/Scripts/Bosses/Vi/ViMovement.cs
@@ -1,8 +1,32 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class ViBossMovement : MonoBehaviour
+/*
+ * Describes the movement patterns of Vi.
+ *
+ * The way Vi moves is by applying motion commands which consist of a direction
+ * and a count that multiplies the effect of the movement. These motion
+ * commands are generated randomly. The commands' behaviour is packaged in 
+ * an inner class. Moreover, they may also be applied to any game object and 
+ * therefore may be used in outside scripts.
+ *
+ * As to how Vi is kept inside the camera's view, the boss has a light copy
+ * of itself called 'ViMovementTester'. The latter is an invisible object with
+ * the same dimensions as Vi. The movement tester interacts only with invisible
+ * walls. The copy executes the generated motion command before Vi and
+ * checks whether it has collided with the invisible encompasing field. If yes,
+ * the motion command is reverted which leads to the boss standing still for 
+ * a moment.
+ */
+public class ViMovement : MonoBehaviour
 {
+    /*
+     * A Vi movement command.
+     *
+     * A movement command is described as having a 'motion' and a 'count'. 
+     * The former is the direction of the movement, whilst the latter is an
+     * integer which multiplies the effect of the command.
+     */
     public class Movement 
     {
         public enum Motion : byte { Left, Down, Up, Right };
@@ -16,6 +40,7 @@ public class ViBossMovement : MonoBehaviour
         };
 
         public Motion motion;
+
         public int count;
 
         public Movement(int minCount, int maxCount)
@@ -24,11 +49,17 @@ public class ViBossMovement : MonoBehaviour
             count = Random.Range(minCount, maxCount + 1);
         }
 
+        /*
+         * Apply the effects of the movement comand.
+         */
         public void Execute(Transform objectTransform, float motionUnit)
         {
             objectTransform.position += motionOffsets[motion] * motionUnit * count;
         }
 
+        /*
+         * Apply the opposite effects of the movement comand.
+         */
         public void ExecuteOpposite(Transform objectTransform, float motionUnit)
         {
             objectTransform.position -= motionOffsets[motion] * motionUnit * count;
@@ -39,6 +70,9 @@ public class ViBossMovement : MonoBehaviour
             return string.Format("{0}{1}", count, GetMotionAsString(motion));
         }
 
+        /*
+         * Pick a random motion.
+         */
         public static Motion PickRandomMotion()
         {
             System.Random random = new System.Random();
@@ -48,6 +82,9 @@ public class ViBossMovement : MonoBehaviour
             return randomMotion;
         }
 
+        /*
+         * Return a letter, representing the motion of the command.
+         */
         public static string GetMotionAsString(Motion motion)
         {
             switch (motion)
@@ -65,15 +102,31 @@ public class ViBossMovement : MonoBehaviour
         }
     }
 
-    public ViBossMovementTester movementTester;
+    // The movement tester - light copy of Vi.
+    public ViMovementTester movementTester;
+
+    // The number of movement commands per second.
     public float movementRate = 6f;
+
+    // The Unity engine units that are traversed in a movement command 
+    // (with count = 1).
     public float motionUnit = 0.5f;
+
+    // Minimum count that may be chosen at random.
     public int minMotionCount = 1;
+
+    // Maximum count that may be chosen at random.
     public int maxMotionCount = 3;
+
+    // The movement to be executed on the next update cycle. Used by the
+    // movement tester to test the movement preemptively.
     [HideInInspector]
     public Movement nextMovement;
 
+    // The period of time in which one movement command is executed.
     private float movementPeriod;
+
+    // Indicates whether another movement command may be generated.
     private bool isInMotion;
 
     void Start()
@@ -82,6 +135,13 @@ public class ViBossMovement : MonoBehaviour
         isInMotion = false;
     }
 
+    /*
+     * Move Vi via a movement command.
+     *
+     * A command may only be generated if the boss is currently not in motion.
+     * The command itself is generated and applied at a random point in time 
+     * within the movement period.
+     */
     void Update()
     {
         float movementDelayTime;
@@ -95,6 +155,14 @@ public class ViBossMovement : MonoBehaviour
         }
     }
 
+    /*
+     * Generate and apply a movement command.
+     *
+     * The new movement command is preemptively applied to the movement tester.
+     * If the latter collides with the hidden collision field, the movement
+     * command is reset and then deleted, resulting in Vi standing still until
+     * the next generated command.
+     */
     private void Move()
     {
         if (nextMovement != null)
@@ -105,6 +173,9 @@ public class ViBossMovement : MonoBehaviour
         movementTester.ApplyMovement();
     }
 
+    /*
+     * Allow another movement command to be generated.
+     */
     private void ResetMotion()
     {
         isInMotion = false;
---
+++ Assets/Scripts/Bosses/Vi/ViMovementTester.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+/*
+ * Detail the behaviour of Vi's movement tester.
+ *
+ * The movement tester is an invisible light copy of Vi with the latter's
+ * physical dimensions. The tester preemptively applies movement commands,
+ * testing whether they may be also applied to the boss itself. If the copy
+ * collides with an invisible wall the movement command is nullified.
+ */
+public class ViMovementTester : MonoBehaviour
+{
+    // Vi - a reference to the boss.
+    public ViMovement vi;
+
+    // The confining movement field of the boss. The latter may not venture
+    // outside of the field.
+    public MovementField movementField;
+    
+    /*
+     * Preemptively execute Vi's next movement command.
+     *
+     * The command is executed on the previous update cycle that Vi would
+     * apply its movement command.
+     */
+    public void ApplyMovement()
+    {
+        FreeMovementField();
+        vi.nextMovement.Execute(transform, vi.motionUnit); 
+    }
+
+    /*
+     * Revert the effects of Vi's next movement command.
+     *
+     * The movement tester is returned to its pre-applied movement command
+     * position. The command itself is nullified and thus - never executed
+     * on Vi.
+     */
+    public void RevertMovement()
+    {
+        LockMovementField();
+        vi.nextMovement.ExecuteOpposite(transform, vi.motionUnit); 
+        vi.nextMovement = null;
+    }
+
+    /*
+     * Allow the movement field to continue following its assigned 
+     * object to be followed.
+     */
+    private void FreeMovementField()
+    {
+        movementField.GetComponent<GameObjectFollower>().ContinueFollowing();
+    }
+
+    /*
+     * Deter the movement field from following its assigned 
+     * object to be followed.
+     */
+    private void LockMovementField()
+    {
+        movementField.GetComponent<GameObjectFollower>().StopFollowing();
+    }
+}
+
