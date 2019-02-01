bfb4832b74b73e323ba63906ffc75f46075d3929
Petar Nikolov <penikolov23@gmail.com>
2019-02-01 12:35:59 +0200

    [Refactoring] Reimplement movement field's follower behaviour
    The 'GameObjectFollower' component has been redesigned into an abstract
    class. It has a few abstract methods which should be implemented by
    subclasses in order to achieve the follower behaviour.
    
    The follower class is implemented by the new 'SmoothDampGameObjectFollower'
    subclass which achieves the follower functionality by smoothly slithering
    towards its target. It also defines a stop zone, which stops the
    follower when entered.

--- Assets/Scripts/GameObjects/GameObjectFollower.cs
+++ Assets/Scripts/GameObjects/GameObjectFollower.cs
@@ -1,65 +1,123 @@
 ﻿using UnityEngine;
 
-public class GameObjectFollower : MonoBehaviour
+/*
+ * Component behaviour to follow another game object.
+ *
+ * Game objects which include this component will constantly follow a 
+ * designated target object. This class implements the logic to determine the
+ * direction in which the follower has to move to close the gap between it and
+ * its target. Furthermore, the follower may be told to follow its target only
+ * vertically or horizontally. How the follower object actually performs the
+ * movement is left to be implemented by a subclass.
+ */
+public abstract class GameObjectFollower : MonoBehaviour
 {
-    public GameObject objectToFollow;
+    // The game object which is to be followed.
+    public GameObject targetObject;
+
+    // Should the game object follow its target on the 'x' axis.
     public bool followXAxis = false;
+
+    // Should the game object follow its target on the 'y' axis.
     public bool followYAxis = false;
 
-    private bool isFollowing = true;
-    private Vector2 followDirection = new Vector2(0, 0);
+    // The moving direction in order to follow the target object. Both x and y
+    // values can be in the set {-1; 0; 1}.
+    protected Vector2 direction = Vector2.zero;
 
+    // Is the game object currently following its target.
+    private bool isFollowing = true;
+
+    /*
+     * Move the follower towards its target.
+     *
+     * Firstly, the direction in which the follower should move in is determined.
+     * The direction is represented as a Vector2 which can hold values in the
+     * following set {-1; 0; 1}. The method only sets the following direction
+     * for an axis if designated via the 'followXAxis' and 'followYAxis'
+     * variables. Afterwards, the follower is moved in the recently determined
+     * direction, closing the distance between it and its target.
+     */
     void FixedUpdate()
     {
+        float distanceToTarget;
+
         if (isFollowing)
         {
             if (followXAxis)
             {
-                if (objectToFollow.transform.position.x - transform.position.x > 0)
+                // Determine direction to follow on the 'x' axis.
+                distanceToTarget = targetObject.transform.position.x - transform.position.x;
+                if (distanceToTarget > 0)
                 {
-                    followDirection.x = 1;
+                    direction.x = 1;
                 }
-                else if (objectToFollow.transform.position.x - transform.position.x < 0)
+                else if (distanceToTarget < 0)
                 {
-                    followDirection.x = -1;
+                    direction.x = -1;
                 }
                 else
                 {
-                    followDirection.x = 0;
+                    direction.x = 0;
                 }
+            }
+            else
+            {
+                direction.x = 0;
             }
             if (followYAxis)
             {
-                if (objectToFollow.transform.position.y - transform.position.y > 0)
+                // Determine direction to follow on the 'y' axis.
+                distanceToTarget = targetObject.transform.position.y - transform.position.y;
+                if (distanceToTarget > 0)
                 {
-                    followDirection.y = 1;
+                    direction.y = 1;
                 }
-                else if (objectToFollow.transform.position.y - transform.position.y < 0)
+                else if (distanceToTarget < 0)
                 {
-                    followDirection.y = -1;
+                    direction.y = -1;
                 }
                 else
                 {
-                    followDirection.y = 0;
+                    direction.y = 0;
                 }
             }
-
-            BroadcastMessage("MoveObjectFollowerInDirection", followDirection);
+            else
+            {
+                direction.y = 0;
+            }
+            MoveFollower();
         }
         else
         {
-            BroadcastMessage("StopObjectFollowerMovement");
+            StopFollower();
         }
     }
 
+    /*
+     * Allow the follower to continue following its target.
+     */
     public void ContinueFollowing()
     {
         isFollowing = true;
     }
 
+    /*
+     * Deter the follower from following its target.
+     */
     public void StopFollowing()
     {
         isFollowing = false;
     }
+
+    /*
+     * Move the follower in the specified direction.
+     */
+    protected abstract void MoveFollower();
+
+    /*
+     * Stop the follower's movement.
+     */
+    protected abstract void StopFollower();
 }
 
---
+++ Assets/Scripts/GameObjects/SmoothDampGameObjectFollower.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+/*
+ * Follow another game object by smoothly sliding towards it.
+ *
+ * This follower implementation adds a stop zone. When the follower reaches
+ * the zone's border, the former stops following its target object.
+ */
+public class SmoothDampGameObjectFollower : GameObjectFollower
+{
+    // The following speed.
+    public float followSpeed = 1f;
+
+    // Range of stop zone. Follower stops following its target when within
+    // 1/2 of this range.
+    public float stopZoneRange = 0.1f;
+
+    // Velocity of follower. This should not be set mannually.
+    private Vector2 velocity = Vector2.zero;
+    
+    /*
+     * Smoothly slither towards the target object.
+     */
+    protected override void MoveFollower()
+    {
+        float distanceToTarget;
+        Vector2 targetPosition;
+
+        distanceToTarget = targetObject.transform.position.x - transform.position.x;
+        if (Math.Abs(distanceToTarget) > stopZoneRange / 2)
+        {
+            // Follow target if outside of stop zone.
+            targetPosition.x = direction.x * Math.Abs(targetObject.transform.position.x);
+        }
+        else
+        {
+            // Stop following target on this axis.
+            targetPosition.x = transform.position.x;
+            StopFollower();
+        }
+        distanceToTarget = targetObject.transform.position.y - transform.position.y;
+        if (Math.Abs(distanceToTarget) > stopZoneRange / 2)
+        {
+            // Follow target if outside of stop zone.
+            targetPosition.y = direction.y * Math.Abs(targetObject.transform.position.y);
+        }
+        else
+        {
+            // Stop following target on this axis.
+            targetPosition.y = transform.position.y;
+            StopFollower();
+        }
+        transform.position = Vector2.SmoothDamp(transform.position, targetPosition, 
+                ref velocity, 1 / followSpeed);
+    }
+
+    /*
+     * Stop following target by nullifying follower's velocity.
+     */
+    protected override void StopFollower()
+    {
+        velocity = Vector2.zero;
+    }
+}
+
